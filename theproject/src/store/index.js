b8e6f91c6a6202e61f3e26864db6c8987add0502
Linghu <391103334@qq.com>
2018-10-29 00:08:22 +0800

    配置store

---
+++ theproject/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import stores from "./modules/stores"
+import service from "./modules/service"
+import goods from "./modules/goods"
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    stores,
+    service,
+    goods
+  },
+  state: {
+
+  },
+  mutations: {
+  },
+  getters: {
+   
+  },
+  actions: {
+   
+  }
+})
---
+++ theproject/src/store/modules/goods.js
@@ -0,0 +1,20 @@
+
+import axios from "axios"
+const state = {
+    goods:[]
+}
+const mutations = {
+}
+const getters = {
+
+}
+const actions = {
+   
+}
+export default {
+    namespaced:true,
+    state,
+    mutations,
+    getters,
+    actions
+}
