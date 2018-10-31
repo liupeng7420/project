0886417905c0a522cd586c555116ef06a7f95f95
Peng <153542598@qq.com>
2018-10-31 12:30:47 +0800

    合并平台管理审核功能

--- theproject/src/store/index.js
+++ theproject/src/store/index.js
@@ -3,14 +3,11 @@ import Vuex from 'vuex'
 import stores from "./modules/stores"
 import service from "./modules/service"
 import goods from "./modules/goods"
-<<<<<<< HEAD
 import indent from "./modules/indent"
 
-=======
 import platform from './modules/platform'
 import closed from './modules/closed'
 import notAudit from './modules/notAudit'
->>>>>>> platfrom
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -18,13 +15,10 @@ export default new Vuex.Store({
     stores,
     service,
     goods,
-<<<<<<< HEAD
-    indent
-=======
+    indent,
     platform,
     closed,
     notAudit
->>>>>>> platfrom
   },
   state: {
 
